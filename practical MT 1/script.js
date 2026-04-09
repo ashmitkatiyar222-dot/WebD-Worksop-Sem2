14f421f5005988bc107076adf17b3c8eb3ac90c2
ashmitkatiyar222-dot <ashmitkatiyar222@gmail.com>
2026-04-09 13:58:18 +0530

    Add files via upload

---
+++ practical MT 1/script.js
@@ -0,0 +1,9 @@
+function calculate() {
+    let p = parseFloat(document.getElementById("principal").value);
+    let r = parseFloat(document.getElementById("rate").value);
+    let t = parseFloat(document.getElementById("time").value);
+
+    let si = (p * r * t) / 100;
+
+    document.getElementById("result").innerHTML = "Simple Interest = " + si;
+}
